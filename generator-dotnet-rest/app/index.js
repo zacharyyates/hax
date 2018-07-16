c09373c36475ece53f6ac235a71d64067609f061
Zachary Yates <zachary.yates@dignari.com>
2018-07-16 10:09:05 -0400

    Added dotnet cli project generation

--- generator-dotnet-rest/app/index.js
+++ generator-dotnet-rest/app/index.js
@@ -34,30 +34,33 @@ module.exports = class extends Generator {
     }
 
     writing() {
-        // scaffold the .Net project
+        // create the project directory
         if (fs.existsSync(this.name)) {
             throw `Directory ${this.name} already exists`;
         }
-
         fs.mkdirSync(this.name);
 
-        // copy files
+        // ignore files
         this.fs.copyTpl(
             this.templatePath('.gitignore'),
             this.destinationPath(`${this.name}/.gitignore`)
         );
+
+        // docker
+        // TODO -- create a docker-compose.yml file or append a service
         this.fs.copyTpl(
             this.templatePath('.dockerignore'),
             this.destinationPath(`${this.name}/.dockerignore`)
         );
-
         this.fs.copyTpl(
             this.templatePath('debug.Dockerfile'),
             this.destinationPath(`${this.name}/debug.Dockerfile`),
             { name: this.name }
         )
 
-
-        // TODO -- create a docker-compose.yml file or append a service
+        // create the project using the dotnet cli
+        this.spawnCommandSync('dotnet', ['new', '-i', 'Amazon.Lambda.Templates']);
+        this.spawnCommandSync('dotnet', ['new', 'serverless.AspNetCoreWebAPI', '-o', this.name]);
+        this.spawnCommandSync('dotnet', ['add', 'package', 'Microsoft.DotNet.Watcher.Tools'], { cwd: `${this.name}/src/${this.name}` });
     }
 };
